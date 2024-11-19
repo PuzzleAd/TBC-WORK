76da7d2ceeb6ec918e26fe761cc300fe2bc39a01
PuzzleAd <mamulashviliirakli65@gmail.com>
2024-11-19 14:20:38 +0400

    adding basic profile Page

--- src/app/(pages)/profile/page.jsx
+++ src/app/(pages)/profile/page.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import Image from 'next/image'
 
 export default function page() {
   return (
-    <div>
-      
+    <div className='w-full h-full flex items-center justify-center gap-6'>
+      <Image 
+      src="/Profile-photo.jpg"
+      alt='Profile-photo'
+      width={200}
+      height={300}
+      className='rounded-lg'
+      />
+      <div className='flex flex-col gap-6'>
+        <h1>Giorgi Isaakadze</h1>
+        <h3>Nickname: GAMER123</h3>
+        <h3>Level: 85</h3>
+        <h3>Armor: Vorpal Light</h3>
+        <h3>Weapon: Mamba Edge Dual Daggers</h3>
+        <h3>Top Dagger on Server</h3>
+      </div>
     </div>
   )
 }
